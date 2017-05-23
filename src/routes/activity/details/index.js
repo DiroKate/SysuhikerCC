3920a58dac5472201f2b03056608ff50d0a14fd9
diro <diro@diromacbook.local>
2017-05-23 22:23:44 +0800

    grid && tile

--- src/routes/activity/details/index.js
+++ src/routes/activity/details/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'dva';
-import { Breadcrumb } from 'antd';
+import { Breadcrumb, Row, Col, Timeline } from 'antd';
 import PropTypes from 'prop-types';
 import styles from './index.less';
 import example from '../../../assets/example.html';
@@ -25,15 +25,51 @@ function Details(props) {
 
 
   return (
-    <div className={styles.content}>
+    <div className={styles.detailsPage}>
       <Breadcrumb style={{ margin: '12px 0', 'font-size': '1.2em' }}>
         <BreadcrumbItem>
           <a href="/activity">活动列表</a>
         </BreadcrumbItem>
         <BreadcrumbItem>活动详情</BreadcrumbItem>
       </Breadcrumb>
+      <Row>
+        <Col span={15} className={styles.content}>
+          <h1>{fakeData.title}</h1>
+          <Row className={styles.content_leader} type="flex" justify="left" align="middle">
+            <Col span={8}>
+              <img
+                src={fakeData.leader_icon}
+                role="presentation"
+              />
+            </Col>
+            <Col>
+              <h2>
+                {fakeData.leader}
+              </h2>
+              <div>{fakeData.create_at}</div>
+            </Col>
+          </Row>
+          <Timeline>
+            <Timeline.Item color="green">Create a services site 2015-09-01</Timeline.Item>
+            <Timeline.Item color="green">Create a services site 2015-09-01</Timeline.Item>
+            <Timeline.Item color="red">
+              <p>Solve initial network problems 1</p>
+              <p>Solve initial network problems 2</p>
+              <p>Solve initial network problems 3 2015-09-01</p>
+            </Timeline.Item>
+            <Timeline.Item>
+              <p>Technical testing 1</p>
+              <p>Technical testing 2</p>
+              <p>Technical testing 3 2015-09-01</p>
+            </Timeline.Item>
+          </Timeline>
+        </Col>
+        <Col span={8} offset={1} className={styles.teamList}>
+          aaaaaa
+        </Col>
+      </Row>
 
-      
+
     </div>
   );
 }
